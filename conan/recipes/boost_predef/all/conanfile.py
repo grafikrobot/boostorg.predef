b894be52bd6af8f7b67f08a9e089f39dc57aa61d
Rene Rivera <grafikrobot@gmail.com>
2025-06-21 14:34:34 -0500

    Fix b2 require version range.

--- conan/recipes/boost_predef/all/conanfile.py
+++ conan/recipes/boost_predef/all/conanfile.py
@@ -10,7 +10,7 @@ class Pkg(ConanFile):
     license = "BSL-1.0"
     url = "https://github.com/boostorg/predef"
 
-    tool_requires = "b2/>=5.3.3.1"
+    tool_requires = "b2/[>=5.3.3.1]"
 
     def export(self):
         git = Git(self, self.recipe_folder)
