35f0b5edcbd4956f3a40b1a922459fdf3f1e8912
Rene Rivera <grafikrobot@gmail.com>
2025-06-22 17:43:40 -0500

    Only do git parts.

--- conan/recipes/boost_predef/all/conanfile.py
+++ conan/recipes/boost_predef/all/conanfile.py
@@ -13,30 +13,30 @@ class Pkg(ConanFile):
     url = "https://github.com/boostorg/predef"
 
     # Requirements
-    python_requires = "b2/[>=5.3.3.1]"
+    # python_requires = "b2/[>=5.3.3.1]"
 
     # Folders and layout
-    no_copy_source = True
+    # no_copy_source = True
 
     # Internal
     _header_only = True
 
-    @property
-    def b2(self):
-        if not hasattr(self, "_b2") or not self._b2:
-            self._b2 = self.python_requires["b2"].B2(self)
-        return self._b2
+    # @property
+    # def b2(self):
+    #     if not hasattr(self, "_b2") or not self._b2:
+    #         self._b2 = self.python_requires["b2"].B2(self)
+    #     return self._b2
 
     def package_id(self):
         if self._header_only:
             self.info.clear()
 
-    def layout(self):
-        # self.b2.layout(header_only=self._header_only)
-        pass
+    # def layout(self):
+    #     # self.b2.layout(header_only=self._header_only)
+    #     pass
 
-    def generate(self):
-        self.b2.generate(header_only=self._header_only)
+    # def generate(self):
+    #     self.b2.generate(header_only=self._header_only)
 
     def export(self):
         git = Git(self, self.recipe_folder)
@@ -46,8 +46,8 @@ class Pkg(ConanFile):
         git = Git(self, self.recipe_folder)
         git.checkout_from_conandata_coordinates()
 
-    def package(self):
-        self.b2.install(target=["install"])
+    # def package(self):
+    #     self.b2.install(target=["install"])
 
     def package_info(self):
         if self._header_only:
